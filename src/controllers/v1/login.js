7616b8c1acba381ab46c1f9ed46a0ff7c5b98ef4
nxhuy1806 <itnxhuy1806@gmail.com>
2022-04-25 07:03:18 +0700

    Finish

--- src/controllers/v1/login.js
+++ src/controllers/v1/login.js
@@ -7,44 +7,45 @@ const router = Router();
 const { User } = models;
 
 router.post(
-  '/login',
-  asyncWrapper(async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.scope('withPassword').findOne({ where: { email } });
+     '/login',
+     asyncWrapper(async (req, res) => {
+          // debugger
+          const { email, password } = req.body;
+          const user = await User.scope('withPassword').findOne({ where: { email } });
 
-    if (!user || !(await user.comparePasswords(password))) {
-      return res
-        .status(401)
-        .send({ success: false, message: 'Invalid credentials' });
-    }
+          if (!user || !(await user.comparePasswords(password))) {
+               return res
+                    .status(401)
+                    .send({ success: false, message: 'Invalid credentials' });
+          }
 
-    const payload = { email };
-    const accessToken = JWTUtils.generateAccessToken(payload);
-    const savedRefreshToken = await user.getRefreshToken();
-    let refreshToken;
+          const payload = { email };
+          const accessToken = JWTUtils.generateAccessToken(payload);
+          const savedRefreshToken = await user.getRefreshToken();
+          let refreshToken;
 
-    if (!savedRefreshToken || !savedRefreshToken.token) {
-      refreshToken = JWTUtils.generateRefreshToken(payload);
+          if (!savedRefreshToken || !savedRefreshToken.token) {
+               refreshToken = JWTUtils.generateRefreshToken(payload);
 
-      if (!savedRefreshToken) {
-        await user.createRefreshToken({ token: refreshToken });
-      } else {
-        savedRefreshToken.token = refreshToken;
-        await savedRefreshToken.save();
-      }
-    } else {
-      refreshToken = savedRefreshToken.token;
-    }
+               if (!savedRefreshToken) {
+                    await user.createRefreshToken({ token: refreshToken });
+               } else {
+                    savedRefreshToken.token = refreshToken;
+                    await savedRefreshToken.save();
+               }
+          } else {
+               refreshToken = savedRefreshToken.token;
+          }
 
-    return res.status(200).send({
-      success: true,
-      message: 'Successfully logged in',
-      data: {
-        accessToken,
-        refreshToken,
-      },
-    });
-  })
+          return res.status(200).send({
+               success: true,
+               message: 'Successfully logged in',
+               data: {
+                    accessToken,
+                    refreshToken,
+               },
+          });
+     })
 );
 
 export default router;
